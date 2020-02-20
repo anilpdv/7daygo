f092c673a262a2ccc29d045869ba403933fe1240
shashank <shashank.s@msr-it.com>
2020-02-20 20:52:51 +0530

    Initial

---
+++ day1-http-base/base2/main.go
@@ -0,0 +1,25 @@
+package main
+
+//Engine : struct
+type Engine struct{}
+
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		fmt.Fprintf(w, "URL.PATH = %q\n", r.URL.Path)
+	case "/hello":
+		for k, v := range r.Header {
+			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+		}
+	default:
+		fmt.Fprintf(w, "404 Not Found %s\n", r.URL)
+	}
+}
+
+func main() {
+	engine := new(Engine)
+	err := http.ListenAndServe(":8888", engine)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
